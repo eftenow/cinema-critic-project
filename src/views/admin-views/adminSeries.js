74996a7adca961631193ee3780404a316080be84
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-05-12 15:33:39 +0300

    added series to the admin panel

--- src/views/admin-views/adminSeries.js
+++ src/views/admin-views/adminSeries.js
@@ -1,7 +1,21 @@
 import { html, render } from '../../../node_modules/lit-html/lit-html.js';
+import { getAllSeries } from '../../services/itemServices.js';
 import { setActiveNavLink } from './adminNavigation.js';
 
-const adminSeriesTemplate = (ctx) => html`
+const seriesTemplateAdmin = (series) => html`
+<tr>  
+      <td>${series.name}</td>
+      <td>${series.genres}</td>
+      <td>${series.rating}</td>
+      <td>${series.visits}</td>
+      <td>
+        <button class='edit-btn-admin'><i class="fas fa-edit"></i></button>
+        <button class='delete-btn-admin'><i class="fas fa-trash"></i></button>
+        <button class='forward-btn-admin'><i class="fa-solid fa-share-from-square"></i></button>
+      </td>
+    </tr>
+`
+const adminSeriesTemplate = (ctx, series) => html`
   <h2 class='admin-table-header'>TV Shows</h2>
   <table class="movie-table">
   <thead>
@@ -14,46 +28,15 @@ const adminSeriesTemplate = (ctx) => html`
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>Movie 1</td>
-      <td>Action</td>
-      <td>8.5 <i class="fa-solid fa-star"></i></td>
-      <td>100</td>
-      <td>
-      <button class='edit-btn-admin'><i class="fas fa-edit"></i></button>
-        <button class='delete-btn-admin'><i class="fas fa-trash"></i></button>
-        <button class='forward-btn-admin'><i class="fa-solid fa-share-from-square"></i></button>
-      </td>
-    </tr>
-    <tr>
-      <td>Movie 2</td>
-      <td>Comedy</td>
-      <td>7.5 <i class="fa-solid fa-star"></i></td>
-      <td>200</td>
-      <td>
-      <button class='edit-btn-admin'><i class="fas fa-edit"></i></button>
-        <button class='delete-btn-admin'><i class="fas fa-trash"></i></button>
-        <button class='forward-btn-admin'><i class="fa-solid fa-share-from-square"></i></button>
-      </td>
-    </tr>
-    <tr>
-      <td>Movie 3</td>
-      <td>Drama</td>
-      <td>9.0 <i class="fa-solid fa-star"></i></td>
-      <td>150</td>
-      <td>
-      <button class='edit-btn-admin'><i class="fas fa-edit"></i></button>
-        <button class='delete-btn-admin'><i class="fas fa-trash"></i></button>
-        <button class='forward-btn-admin'><i class="fa-solid fa-share-from-square"></i></button>
-      </td>
-    </tr>
+  ${series.map(currentSeries => seriesTemplateAdmin(currentSeries))}
   </tbody>
 </table>
 
 `
 
-export function renderSeriesAdmin(ctx) {
-    const siteSeries = adminSeriesTemplate();
+export async function renderSeriesAdmin(ctx) {
+    const series = await getAllSeries();
+    const siteSeries = adminSeriesTemplate(ctx, series);
     setActiveNavLink('/admin/series');
     ctx.render(siteSeries);
 }
