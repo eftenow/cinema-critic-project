7d9468cb6d704328a3da593f5a11e027bdb7169b
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-05-20 15:06:34 +0300

    css changes to the home page

--- src/views/home.js
+++ src/views/home.js
@@ -3,6 +3,9 @@ import { html } from '../../node_modules/lit-html/lit-html.js';
 
 const homeTempalte = () => html`
 <div class="home-page">
+    <div class='home-page-background'>
+        <img src='../../images/marvel-endgame.jpg' class='home-page-background-img'>
+    </div>
     <section class="about-section">
         <h1>Cinema Critic</h1>
         <h3>The ultimate platform for movie and TV show enthusiasts to share their
@@ -11,7 +14,7 @@ const homeTempalte = () => html`
         <article>Join our community of film and TV lovers and share your thoughts on the latest
             blockbusters, hidden
             gems, and classic favorites. Start exploring Cinema Critic today and let your voice be heard!
-            zzz
+            
         </article>
         <a href="/dashboard" class="button start-btn">Get Started</a>
     </section>
